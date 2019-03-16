78e8dc4374e688e9a19896c289ee2db8adab43b3
Leandro <leaxviana140@gmail.com>
2019-03-16 15:12:17 -0400

    add node

---
+++ Binary Search Tree.js
@@ -0,0 +1,40 @@
+class Node {
+	constructor(data, left = null, right = null) {
+		this.data = data;
+		this.left = left;
+		this.right = right
+	}
+}
+
+class Tree {
+	constructor() {
+		this.root = null;
+	}
+
+	add(data) {
+		const node = this.root;
+		if (node === null) {
+			this.root = new Node(data);
+			return;
+		} else{
+			const searchTree = (data) =>{
+				if (data < node.data){
+					if(node.left === null){
+						node.left = new Node(data);
+						return;
+					} else if(node.left !== null){
+						return searchTree(node.left);
+					}
+				} else if(data > node.data){
+					if(node.right === null){
+						node.left = new Node(data);
+						return;
+					}
+				} else{
+					return null;
+				}
+			};
+			return searchTree(node);
+		}
+	}
+}
